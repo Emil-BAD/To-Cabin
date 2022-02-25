05b2e704ec1532c2bc9b4ca2df862e894cae2026
Emil Badretdinov <hat-patfaceit@mail.ru>
2022-02-25 20:25:20 +0300

    Prof_List

--- server.py
+++ server.py
@@ -9,5 +9,10 @@ def index(prof):
     return render_template('base.html', prof=prof)
 
 
+@app.route('/list_prof/<value>')
+def prof_list(value):
+    return render_template('prof_list.html', value=value)
+
+
 if __name__ == '__main__':
     app.run(port=8000, host='127.0.0.1')
